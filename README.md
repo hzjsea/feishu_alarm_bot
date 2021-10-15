e3e8399f392ae691c85d5237fbe1322a2d6e367b
hzjsea <hzjsea@gmail.com>
2021-10-15 14:00:54 +0800

    update setup.py

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # feishu_alarm_bot
 飞书报警机器人 支持Yaml配置报警信息
+
+> 机器人分为两种机器人，一种是无法提供交互环境的 只能作为监控出现 ,另外一种是可以提供交互环境, 可以设置不同的任务需求，更加灵活，但需要提供一个回调地址， 前者叫做自定义机器人
+
+具体可以看这里
+https://open.feishu.cn/document/ukTMukTMukTM/ucTM5YjL3ETO24yNxkjN
+
+## 基于会话的机器人
+创建应用文档
+https://open.feishu.cn/document/home/interactive-session-based-robot/introduction
+开发者平台
+https://open.feishu.cn/app/cli_a0f784bae739900e/overview
+
+添加回调地址
+![](https://noback.upyun.com/2021-10-14-16-07-50.png!)
+
+权限管理
+![](https://noback.upyun.com/2021-10-14-16-10-30.png!)
+
+事件订阅 回调地址和前面一样
+![](https://noback.upyun.com/2021-10-14-16-11-01.png!)
+
+开发文档
+https://open.feishu.cn/document/home/develop-a-bot-in-5-minutes/coding
+https://sf3-cn.feishucdn.com/obj/website-img/093b18e5189ad423a378b616b1bf94c3_Tbosxo7mus.py
+
+消息卡片定义
+https://open.feishu.cn/tool/cardbuilder?from=ttrlbot3
+
+json to yaml
+https://onlineyamltools.com/convert-json-to-yaml
+
+
+
+## 权限管理
+
+APP_ID  和 APP_SECRET 在`凭证与基础信息`获得
+APP_VERIFICATION_TOKEN 在 `事件订阅` 中获得
---
+++ common/bot.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+@author: hzjsea
+@file: bot.py
+@time: 2021/10/15 10:59 上午
+"""
+from enum import Enum
+from utils import JSONType
+
+
+class MethodEnum(Enum):
+    POST = 'POST'
+    GET = 'GET'
+
+
+def switch_type(type: str, open_id: str) -> tuple[str, JSONType, MethodEnum]:
+    url = ""
+    req_body = {}
+    method = MethodEnum.POST
+
+    if type == "alarm":
+        pass
+    elif type == "chat":
+        # 获取群号， 但是在聊天 send接口中也有 对应的是 chat_id 字段 必须放在req_body{} 一层下面
+        # url = "https://open.feishu.cn/open-apis/chat/v4/list"
+        # self.send_request(url=url, headers=headers, data=None,method=MethodEnum.GET)
+
+        url = "https://open.feishu.cn/open-apis/message/v4/send/"
+        req_body = {
+            "open_id": open_id,
+            "msg_type": "post",
+            # "chat_id":"oc_75073e8c0265baab97ddb0082f4b260c",
+            "content": {
+                "post": {
+                    "zh_cn": {
+                        # "title": "无对应指令,指令列表如下",
+                        "content": [
+                            [
+                                {
+                                    "tag": "text",
+                                    "un_escape": True,
+                                    "text": "你好"
+                                },
+                                {
+                                    "tag": "at",
+                                    # "user_id": "ou_21cdbb2f082181e4bd7e1625fcfd1082" # open_id
+                                    "user_id": open_id
+                            }
+                            ]
+                        ]
+                    },
+
+                }
+            }
+        }
+        method = MethodEnum.POST
+    elif type == "normal":
+        url = "https://open.feishu.cn/open-apis/message/v4/send/"
+        req_body = {
+            "open_id": open_id,
+            "msg_type": "post",
+            "content": {
+                "post": {
+                    "zh_cn": {
+                        "title": "无对应指令,指令列表如下",
+                        "content": [
+                            [
+                                {
+                                    "tag": "text",
+                                    "un_escape": True,
+                                    "text": "wm tt ct"
+                                }
+                            ]
+                        ]
+                    },
+
+                }
+            }
+        }
+        method = MethodEnum.POST
+    elif type == "test":
+        url = "https://open.feishu.cn/open-apis/message/v4/send/"
+        req_body = {
+            "open_id": open_id,
+            "msg_type": "post",
+            "content": {
+                "post": {
+                    "zh_cn": {
+                        "title": "我是一个标题",
+                        "content": [
+                            [
+                                {
+                                    "tag": "text",
+                                    "text": "着火啦"
+                                },
+                            ],
+                        ]
+                    }
+                }
+            }
+        }
+        method = MethodEnum.POST
+    return url, req_body, method
