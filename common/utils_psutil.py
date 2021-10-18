d44d55ac4f78b6357123dd2d42b89ba9bb2f9c75
hzjsea <hzjsea@gmail.com>
2021-10-18 14:13:37 +0800

    update

---
+++ common/utils_psutil.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+@author: hzjsea
+@file: utils_psutil.py.py
+@time: 2021/10/18 2:12 下午
+"""
--- setting/development_config.py
+++ setting/development_config.py
@@ -10,9 +10,9 @@ from pydantic import BaseSettings
 
 
 class Config(BaseSettings):
-    APP_ID: str = "cli_a0f784bae739900e"
-    APP_SECRET: str = "doG12QNrsWTTB20zQMmHmhCrxhgBdSPU"
-    APP_VERIFICATION_TOKEN: str = "BlkeeevdFlYi8dj9IJGXAbGYmbdrFNCo"
+    APP_ID: str = ""
+    APP_SECRET: str = ""
+    APP_VERIFICATION_TOKEN: str = ""
 
 
 config = Config()
